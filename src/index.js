9f30add0ba86ab3c419da2e1013f33d03285572f
Gaurav Agarwal <82750742+gaurav-101@users.noreply.github.com>
2023-01-30 11:53:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./Components/App.jsx"
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById("root")
+// );
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
